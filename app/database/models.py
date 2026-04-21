fbfcb51c0018144cddc6b6b5e8bfa0e401524abc
ashxvegeta <ashutoshroy453@gmail.com>
2026-04-21 21:43:55 +0530

    created tables and models

---
+++ app/database/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from app.database.db import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    skill_name = Column(String, nullable=False)
+    task_text = Column(String, nullable=False)
+    status = Column(String, default="pending")
