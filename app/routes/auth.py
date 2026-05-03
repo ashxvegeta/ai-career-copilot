ac238c7a9232e742a5494d2204d0fb8da803b0d9
ashxvegeta <ashutoshroy453@gmail.com>
2026-05-03 20:39:03 +0530

    Add /me Endpoint

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
 from app.services.auth_service import register_user, login_user
 from app.schemas.auth_schema import UserAuth
+from app.utils.dependencies import get_current_user
+from app.database.models import User
+from fastapi import Depends
 
 router = APIRouter()
 
@@ -13,3 +16,13 @@ def register(data: UserAuth):
 @router.post("/login")
 def login(data: UserAuth):
     return login_user(data.email, data.password)
+
+
+@router.get("/me")
+def get_me(
+    current_user: User = Depends(get_current_user)
+):
+    return {
+        "id": current_user.id,
+        "email": current_user.email
+    }
